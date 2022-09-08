68698c97349f26cda242990477de284d0f79e5fb
Cuong Nguyen <cuongnguyen3900@gmail.com>
2022-09-09 00:01:59 +0700

    sterophonics

---
+++ comment.js
@@ -0,0 +1,14 @@
+//Exercise 7:
+//1. Understand
+// define a function named "toUpper" taking 1 parameter
+// array of strings - (strArr)
+// then console.log a new array with all strings capitalized
+
+//2. understand
+//Take one string in the array
+//push it one more time in the same array
+
+//3.
+// let total = 0
+// start from the first number (forEach)
+// keep the previous added number
